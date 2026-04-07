e0a01966a2d7ceeee8344d755702116a91f4a9f9
Juan Mazzola <juan.mazzola@gmail.com>
2026-04-06 22:17:47 -0300

    PWA: usar ícono SVG en lugar de PNG (evita problemas de deploy de binarios)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\IconController;
 use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
-
-Route::get('/icons/icon-{size}.png', IconController::class)->where('size', '192|512');
