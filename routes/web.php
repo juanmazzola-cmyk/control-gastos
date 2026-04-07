d4ae31761736cba4ad1b39a17e5dc5859a72716b
Juan Mazzola <juan.mazzola@gmail.com>
2026-04-06 22:47:27 -0300

    PWA: íconos via controlador, limpiar scripts temporales

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\IconController;
 use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+Route::get('/icons/icon-{size}.png', IconController::class)->where('size', '192|512');
