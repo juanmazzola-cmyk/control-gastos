23c098e2e161688b66cc3d6ccf35ccab5e236053
Juan Mazzola <juan.mazzola@gmail.com>
2026-04-06 22:34:54 -0300

    version.php: generar íconos via web si no existen

--- public/version.php
+++ public/version.php
@@ -1,1 +1,21 @@
-<?php echo "commit: 6339936 - " . date('Y-m-d H:i:s');
+<?php
+$dir = __DIR__ . '/icons';
+if (!is_dir($dir)) mkdir($dir, 0755, true);
+
+foreach ([192, 512] as $size) {
+    $file = $dir . '/icon-' . $size . '.png';
+    if (!file_exists($file)) {
+        $img = imagecreatetruecolor($size, $size);
+        $bg  = imagecolorallocate($img, 79, 70, 229);
+        $fg  = imagecolorallocate($img, 255, 255, 255);
+        imagefilledellipse($img, $size / 2, $size / 2, $size, $size, $bg);
+        $font = 5;
+        $x = (int)(($size - imagefontwidth($font)) / 2);
+        $y = (int)(($size - imagefontheight($font)) / 2);
+        imagestring($img, $font, $x, $y, '$', $fg);
+        imagepng($img, $file);
+        imagedestroy($img);
+    }
+}
+
+echo "OK";
